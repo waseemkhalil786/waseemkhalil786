5c6a0222634d81b15abc23a1f80481a2f2d4b190
Waseem khalil world <156056064+waseemkhalil786@users.noreply.github.com>
2024-02-23 17:42:09 +0500

    JS Assigntments 01
    Continuoes practice

---
+++ JS Assingments/Java.js
@@ -0,0 +1,29 @@
+// User put in values
+let num1 = prompt("Enter 1 value");
+let num2 = prompt("Enter 2 value");
+
+// String change into number
+num1= Number(num1);
+num2= Number(num2);
+
+// Adition value
+let sum = num1+num2;
+alert("Adition:"+ sum);
+console.log ("Adition:"+ sum);
+
+// Subtraction value
+let minus = num1-num2;
+alert("Subtraction:"+ minus);
+console.log ("Subtraction:"+ minus);
+
+// Multiply value
+let Multiply = num1*num2;
+alert("Multiply:"+ Multiply);
+console.log ("Multiply:"+ Multiply);
+
+//Divide vlaue
+let Divide = num1/num2;
+alert("Divide:"+ Divide);
+console.log ("Divide:"+ Divide);
+
+alert("Thankyou😊 ")
